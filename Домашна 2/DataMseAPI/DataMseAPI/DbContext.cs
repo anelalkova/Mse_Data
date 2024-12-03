033ffafd2ffc2300af0d2eb70bd9db4eb02c4c84
Anastasija Lalkova <anastasija.lalkova@gmail.com>
2024-12-03 22:56:36 +0100

    Created API

---
+++ Домашна 2/DataMseAPI/DataMseAPI/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataMseAPI.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using DataMseAPI.Model;
+
+namespace DataMseAPI.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<MseData> MseData { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ Домашна 2/DataMseAPI/DataMseAPI/Repository/MseDataRepository.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using DataMseAPI.Model;
+using DataMseAPI.Models;
+using DataMseAPI.Repository.Interface;
+using Microsoft.Extensions.Options;
+using Npgsql;
+using System.Runtime;
+
+namespace DataMseAPI.Repository
+{
+    public class MseDataRepository : IMseDataRepository
+    {
+        private readonly DbSettings _dbSettings;
+        public MseDataRepository(IOptions<DbSettings> dbSettings)
+        {
+            _dbSettings = dbSettings.Value;
+        }
+
+        public async Task<IEnumerable<MseData>> GetAllDataAsync()
+        {
+            using (var connection = new NpgsqlConnection(_dbSettings.PostgreSQL))
+            {
+                await connection.OpenAsync();
+                string sql = "SELECT * FROM MseData";
+
+                var result = await connection.QueryAsync<MseData>(sql);
+
+                return result;
+            }
+        }
+        
+        public async Task<IEnumerable<String>> GetCodesAsync()
+        {
+            using (var connection = new NpgsqlConnection(_dbSettings.PostgreSQL))
+            {
+                await connection.OpenAsync();
+                string sql = "SELECT Code FROM MseData";
+
+                var result = await connection.QueryAsync<String>(sql);
+
+                return result;
+            }
+        }
+
+        public async Task<IEnumerable<MseData>> GetDataByCodeAsync(String code)
+        {
+            using (var connection = new NpgsqlConnection(_dbSettings.PostgreSQL))
+            {
+                await connection.OpenAsync();
+                string sql = "SELECT * FROM MseData WHERE code = @code";
+
+                var result = await connection.QueryAsync<MseData>(sql, new { code });
+
+                return result;
+            }
+        }
+    }
+}
